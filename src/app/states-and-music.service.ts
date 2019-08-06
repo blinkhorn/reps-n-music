f6f0070b9ebfdb8c217313fd013889616728e071
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-08-06 17:42:37 -0400

    moved logic into states-and-music service to keep DRY

--- src/app/states-and-music.service.ts
+++ src/app/states-and-music.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import countyData from '../assets/data.json';
 @Injectable({
   providedIn: 'root'
 })
@@ -57,6 +57,61 @@ export class StatesAndMusicService {
     'Wisconsin',
     'Wyoming'
   ];
+
+  statesHash = {
+    Alabama: 'AL',
+    Alaska: 'AK',
+    Arizona: 'AZ',
+    Arkansas: 'AR',
+    California: 'CA',
+    Colorado: 'CO',
+    Connecticut: 'CT',
+    Delaware: 'DE',
+    Florida: 'FL',
+    Georgia: 'GA',
+    Hawaii: 'HI',
+    Idaho: 'ID',
+    Illinois: 'IL',
+    Indiana: 'IN',
+    Iowa: 'IA',
+    Kansas: 'KS',
+    Kentucky: 'KY',
+    Louisiana: 'LA',
+    Maine: 'ME',
+    Maryland: 'MD',
+    Massachusetts: 'MA',
+    Michigan: 'MI',
+    Minnesota: 'MN',
+    Mississippi: 'MS',
+    Missouri: 'MO',
+    Montana: 'MT',
+    Nebraska: 'NE',
+    Nevada: 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    Ohio: 'OH',
+    Oklahoma: 'OK',
+    Oregon: 'OR',
+    Pennsylvania: 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    Tennessee: 'TN',
+    Texas: 'TX',
+    Utah: 'UT',
+    Vermont: 'VT',
+    Virginia: 'VA',
+    Washington: 'WA',
+    'West Virginia': 'WV',
+    Wisconsin: 'WI',
+    Wyoming: 'WY'
+  };
+
+  counties = countyData;
   constructor() {}
 
   setState(stateValue: string) {
@@ -69,5 +124,14 @@ export class StatesAndMusicService {
 
   isValidState(): boolean {
     return this.states.includes(this.getState());
+  }
+
+  getStateCounties(state: string): string[] {
+    const countiesArr = Object.keys(this.counties);
+    return countiesArr.filter(item => item.substring(0, 2) === this.statesHash[state]);
+  }
+
+  getTooltipInfo(county: string): string {
+    return `${this.counties[county].fullName}, ${this.counties[county].raceRatingSegment}`;
   }
 }
