236c088e8002e6284af14ba181302ab6e841d3cc
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-08-18 16:50:33 -0400

    refactor county-compare-select

--- src/app/county-compare-select/county-compare-select.component.ts
+++ src/app/county-compare-select/county-compare-select.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { StatesAndMusicService } from '../states-and-music.service';
 
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-county-compare-select',
   templateUrl: './county-compare-select.component.html',
   styleUrls: ['./county-compare-select.component.css']
 })
 export class CountyCompareSelectComponent implements OnInit, OnDestroy {
-
-  previousCountyName: string;
-  private sub: any;
-  constructor(private route: ActivatedRoute, private statesAndMusicService: StatesAndMusicService) { }
+  private previousCountyName: string;
+  private sub = new Subscription();
+  constructor(
+    private route: ActivatedRoute,
+    private statesAndMusicService: StatesAndMusicService
+  ) {}
 
   ngOnInit() {
-    this.sub = this.route.params.subscribe( params => {
-      this.previousCountyName = params.id;
-    });
+    this.sub.add(
+      this.route.params.subscribe(params => {
+        this.previousCountyName = params.id;
+      })
+    );
   }
 
   getState(): string {
@@ -31,9 +36,11 @@ export class CountyCompareSelectComponent implements OnInit, OnDestroy {
     return this.statesAndMusicService.getTooltipInfo(county);
   }
 
+  getPreviousCountyName(): string {
+    return this.previousCountyName;
+  }
+
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
-
-
 }
