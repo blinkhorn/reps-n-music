36ceb174d968a6bc8f451c9753f94f6a8103e18e
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2019-08-18 18:23:20 -0400

    possibility for selecting counties in big states

--- src/app/county-select/county-select.component.ts
+++ src/app/county-select/county-select.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
-
 import { StatesAndMusicService } from '../states-and-music.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 @Component({
   selector: 'app-county-select',
   templateUrl: './county-select.component.html',
   styleUrls: ['./county-select.component.css']
 })
 export class CountySelectComponent implements OnInit {
+  private countyControl = new FormControl();
 
-  constructor(private statesAndMusicService: StatesAndMusicService) { }
+  private countyOptions: string[] = this.getStateCounties(this.getState());
 
-  ngOnInit() {}
+  private filteredCountyOptions: Observable<string[]>;
+
+  constructor(private statesAndMusicService: StatesAndMusicService) {}
+
+  ngOnInit() {
+    this.filteredCountyOptions = this.countyControl.valueChanges.pipe(
+      startWith(''),
+      map(county => this.filterCounties(county))
+    );
+  }
+
+  private filterCounties(county: string): string[] {
+    const filterValue = county.toLowerCase();
+
+    return this.countyOptions.filter(option =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 
   getState(): string {
     return this.statesAndMusicService.getState();
@@ -25,4 +45,11 @@ export class CountySelectComponent implements OnInit {
     return this.statesAndMusicService.getTooltipInfo(county);
   }
 
+  getFilteredCountyOptions(): Observable<string[]> {
+    return this.filteredCountyOptions;
+  }
+
+  getCountyControl(): FormControl {
+    return this.countyControl;
+  }
 }
